d4f95c1e2ad9932099d1e924fa46a433eca76e8b
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2024-02-18 14:27:14 +0800

    update navbar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -7,17 +7,23 @@ function NavBar() {
       <Link href="/">
         <h1 className="font-bold text-xl">UNISPORTS</h1>
       </Link>
-      <nav className="flex flex-row  flex-auto">
-        <ul className="flex flex-row flex-auto gap-8">
+      <nav className="flex flex-row flex-auto justify-end">
+        <ul className="hidden md:flex md:flex-row md:flex-auto md:gap-8">
           <NavItem href="/" name="Home" />
           <NavItem href="/men" name="Men" />
           <NavItem href="/women" name="Women" />
           <NavItem href="/kids" name="Kids" />
         </ul>
         <ul className="flex flex-row gap-5 items-center">
-          <NavItem href="/login" name="Login" isBold={true} />
+          <NavItem
+            href="/login"
+            name="Login"
+            isBold={true}
+            hideOnMobile={true}
+          />
           <NavItem href="/search" icon="search.svg" />
           <NavItem href="/cart" icon="shopping-cart.svg" />
+          <img className="md:hidden" src={`/icons/menu.svg`} alt="menu" />
         </ul>
       </nav>
     </header>
@@ -29,11 +35,13 @@ interface NavItemProps {
   name?: string;
   icon?: string;
   isBold?: boolean;
+  hideOnMobile?: boolean;
 }
-function NavItem({ href, name, icon, isBold }: NavItemProps) {
+function NavItem({ href, name, icon, isBold, hideOnMobile }: NavItemProps) {
   const classNames = `uppercase text-lg ${isBold ? 'font-bold' : 'font-light'} hover:font-bold hover:underline hover:underline-offset-8 hover:decoration-2`;
+  const hideOnMobileClassName = `${hideOnMobile ? 'hidden md:flex' : ''}`;
   return (
-    <li>
+    <li className={hideOnMobileClassName}>
       <Link href={href} className={classNames}>
         {icon && <img src={`/icons/${icon}`} alt={name} />} {!icon && name}
       </Link>
