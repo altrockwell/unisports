1a34f55acf2ecc8bf503ec669ee565eeca3b4f8e
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2024-02-19 16:56:39 +0800

    update hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 function HomePage() {
   return (
-    <div className=" ">
-      <h1 className="absolute left-[5vw] top-20  z-10 font-bold uppercase text-end w-96 text-6xl">
+    <div className=" wrapper">
+      <h1 className="absolute top-20  z-[100] font-bold uppercase text-end hero-text">
         Meet New Summer Wave
       </h1>
       <div className="absolute max-h-[80vh] top-20 right-0 md:right-[5vw] left-[20vw] bottom-14">
