eed51ba59dd0dc57341da75b0c388fdf65d34728
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2024-02-21 10:43:01 +0800

    update hero section's responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 function HomePage() {
   return (
     <div className=" wrapper">
-      <h1 className="absolute top-20 z-[100] font-bold uppercase text-end hero-text">
+      <h1 className="absolute top-20 z-[3] font-bold uppercase text-end hero-text">
         Meet New Summer Wave
       </h1>
-      <div className="absolute max-h-[80vh] top-20 right-0 lg:right-[5vw] left-[20vw] bottom-14">
+      <div className="absolute max-h-[80vh] top-20 right-0 lg:right-[5vw] left-[20vw] 2xl:left-[15vw] bottom-14">
         <Image
           src="/pictures/people-running.jpg"
           alt="people running"
@@ -17,12 +17,12 @@ function HomePage() {
         />
       </div>
       <div>
-        <div className="absolute border-black border-t-2 w-[30vw] bottom-[7vh] left-[12vw]" />
-        <div className="absolute border-black border-l-2  h-[73vh] bottom-[7vh] left-[12vw] " />
-        <div className="triangle-right absolute left-[42vw] bottom-[7vh] translate-y-1/2 "></div>
+        <div className="absolute border-black border-t-2 w-[30vw] md:w-[55vw] lg:w-[65vw] 2xl:w-[71vw] bottom-[7vh] lg:bottom-[5vh] 2xl:bottom-[8vh] left-[12vw] 2xl:left-[11vw]" />
+        <div className="absolute border-black border-l-2  h-[73vh] lg:h-[65vh] 2xl:h-[70vh] bottom-[7vh] lg:bottom-[5vh] left-[12vw] 2xl:bottom-[8vh] 2xl:left-[11vw]" />
+        <div className="triangle-right absolute left-[42vw] md:left-[67vw] lg:left-[77vw] 2xl:left-[82vw] bottom-[7vh] lg:bottom-[5vh] 2xl:bottom-[8vh] translate-y-1/2 "></div>
       </div>
       <button
-        className="border-black border-2 text-[28px] uppercase font-bold absolute bottom-[7vh] translate-y-1/2 right-[5vw] px-2"
+        className="border-black border-2 text-[28px] 2xl:text-[40px] uppercase font-bold absolute bottom-[7vh] lg:bottom-[5vh] 2xl:bottom-[8vh] translate-y-1/2 right-[5vw] px-2"
         type="button"
       >
         Shop Now!
